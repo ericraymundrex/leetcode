3bcf9ef8755722b1178150d19674a97619b5891d
Eric Raymund Rex Sylvester <ericrexs8@gmail.com>
2022-02-28 12:08:08 +0530

    Create two_sum.py

---
+++ Python/two_sum.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+
+    # Brute Force solution
+    # --------------------------------------------------
+    def twoSum_brute_force(self, nums: List[int], target: int) -> List[int]:
+        length=len(nums)
+        for x in range(length):
+            for y in range(x+1,length):
+                if nums[x]+nums[y] == target:
+                    return [x,y]
+        return []
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.twoSum_brute_force([3, 2, 4], 6))
